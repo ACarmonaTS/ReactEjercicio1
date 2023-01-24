6bc7c97ca16f8d07366b6c475b17cd5b2d243b17
Raul Gonzalez <rauls.gh24@gmail.com>
2023-01-24 14:16:39 -0600

    Utilizar estados

--- circe-react/src/App.js
+++ circe-react/src/App.js
@@ -3,99 +3,92 @@ import React from "react";
 import * as Names from "./components/resources/NameImage.js";
 import Card from "./components/Card/Card.js";
 import UnsplashImage from "./components/UnsplashImage/UnsplashImage.js";
-import * as interaction from './interaction.js'
-
-setTimeout( () => {
-  console.clear(interaction.animations)
-}, 100)
 
 function App() {
   return (
     <React.Fragment>
-      <header><h1>HEADER</h1></header>
+      <header>
+        <h1>HEADER</h1>
+      </header>
       <main>
         <div>
-          { Names.blockOneImage.map( (image) => {
-            console.log(image)
+          {Names.blockOneImage.map((image) => {
             return (
               <Card key={image.alt}>
                 <UnsplashImage alt={image.alt} src={image.scr} />
               </Card>
-            )
+            );
           })}
         </div>
         <div>
-          { Names.blockTwoImage.map( (image) => {
-            console.log(image)
+          {Names.blockTwoImage.map((image) => {
             return (
               <Card key={image.alt}>
                 <UnsplashImage alt={image.alt} src={image.scr} />
               </Card>
-            )
+            );
           })}
         </div>
         <div>
-          { Names.blockOneImage.map( (image) => {
-            console.log(image)
+          {Names.blockOneImage.map((image) => {
+            console.log(image);
             return (
               <Card key={image.alt}>
                 <UnsplashImage alt={image.alt} src={image.scr} />
               </Card>
-            )
+            );
           })}
         </div>
         <div>
-          { Names.blockTwoImage.map( (image) => {
-            console.log(image)
+          {Names.blockTwoImage.map((image) => {
             return (
               <Card key={image.alt}>
                 <UnsplashImage alt={image.alt} src={image.scr} />
               </Card>
-            )
+            );
           })}
         </div>
         <div>
-          { Names.blockOneImage.map( (image) => {
-            console.log(image)
+          {Names.blockOneImage.map((image) => {
             return (
               <Card key={image.alt}>
                 <UnsplashImage alt={image.alt} src={image.scr} />
               </Card>
-            )
+            );
           })}
         </div>
         <div>
-          { Names.blockTwoImage.map( (image) => {
-            console.log(image)
+          {Names.blockTwoImage.map((image) => {
+            console.log(image);
             return (
               <Card key={image.alt}>
                 <UnsplashImage alt={image.alt} src={image.scr} />
               </Card>
-            )
+            );
           })}
         </div>
         <div>
-          { Names.blockOneImage.map( (image) => {
-            console.log(image)
+          {Names.blockOneImage.map((image) => {
             return (
               <Card key={image.alt}>
                 <UnsplashImage alt={image.alt} src={image.scr} />
               </Card>
-            )
+            );
           })}
         </div>
         <div>
-          { Names.blockTwoImage.map( (image) => {
-            console.log(image)
+          {Names.blockTwoImage.map((image) => {
             return (
               <Card key={image.alt}>
                 <UnsplashImage alt={image.alt} src={image.scr} />
               </Card>
-            )
+            );
           })}
         </div>
       </main>
-      <footer><h2>FOOTER</h2></footer>
+      <footer>
+        <h2>FOOTER</h2>
+      </footer>
     </React.Fragment>
   );
 }
--- circe-react/src/components/UnsplashImage/UnsplashImage.js
+++ circe-react/src/components/UnsplashImage/UnsplashImage.js
@@ -1,5 +1,17 @@
-function UnsplashImage(props){
-    return <img src={ props.src} alt={props.alt}/>
+import { useState } from "react";
+
+function UnsplashImage(props) {
+  const [animation, setAnimation] = useState(false);
+  
+  return (
+    <img
+    className={animation ? "img-hover": "main div div"}
+      src={props.src}
+      alt={props.alt}
+      onMouseLeave={() => setAnimation(false)}
+      onMouseEnter={() => setAnimation(true)}
+    />
+  );
 }
 
-export default UnsplashImage
+export default UnsplashImage;
